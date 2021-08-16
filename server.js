12b087ce8a810eacb20503931540651c5bfb483c
Vivi Cowan <vivicare@gmail.com>
2021-08-15 19:08:32 -0700

    testing

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+// || "mongodb://localhost/budget"
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
