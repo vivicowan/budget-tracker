aad41b6055c5712fc604e067cbe2af8093046e57
Vivi Cowan <vivicare@gmail.com>
2021-08-15 17:46:52 -0700

    removing waituntil data_cache_name

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,23 +1,18 @@
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/styles.css",
-  "/index.js",
   "/db.js",
+  "/index.js",
   "/manifest.webmanifest",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
 ];
 
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 // install
 self.addEventListener("install", function (evt) {
-  // pre cache image data
-  evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
-  );
-
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
